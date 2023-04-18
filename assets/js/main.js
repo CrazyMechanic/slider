cbaa1ca3cecf307d5d5b9beed9d4ef0219c9e2b5
CrazyMechanic <egortimchenko94@gmail.com>
2023-04-18 21:24:41 +0300

    dev first end

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,3 +131,10 @@
 
   initApp();
 }());
+
+$(document).ready(() => {
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('-active');
+    $('.header-navbar__list').toggleClass('-active');
+  });
+});
